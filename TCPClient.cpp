ea8766841ade27735cf8b35bebec3d87b5ecd6a4
AnsahOfei <ansahofei.kwadwo@outlook.com4>
2025-07-05 21:31:45 +0000

    Added definitions for some functions of the TCPClient for the moment

--- TCPClient.cpp
+++ TCPClient.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -21,3 +22,55 @@ class TCPClient{
         sockaddr_in server_addr;
 
 };
+
+//COnstructor for creating the TCP client socket
+ TCPClient::TCPClient(){
+    sock_fd = socket(AF_INET, SOCK_STREAM,IPPROTO_TCP);
+    if (sock_fd < 0){
+        //to be replaced with exception later
+        perror("socket() failed");
+    }
+ }
+
+ //Destructor for closing socket
+ TCPClient::~TCPClient(){
+    close(sock_fd);
+ }
+//Define function to connect to server
+bool TCPClient::connectToServer(const std::string& serverIp, int port){
+    //Zero out the struct
+    memset(&server_addr, 0, sizeof(server_addr));
+    
+    //Fill server address details
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port); // Convert port number to network bytes foonerm
+
+    //Convert IP address from ip string to binary form
+    int result = inet_pton(AF_INET, serverIp.c_str(),&server_addr.sin_addr);
+    if (result <= 0){ //catch errors
+        perror("inet_pton() failed");
+        return false;
+    }
+
+    //Connect to server and check for error in connection
+    if(connect(sock_fd, (sockaddr*)&server_addr,sizeof(server_addr)<0)){
+        perror("connect() failed");
+        return false;
+    }
+
+    //If operations were successful
+    return true;
+
+}
+
+void TCPClient::sendMessage(const std::string& message){
+    //send() returns the number of bytes sent after it is invoked
+    ssize_t numBytesSent = send(sock_fd,message.c_str(),message.length(),0);
+    if (numBytesSent<0){
+        perror("send() failed");
+    } else if (numBytesSent != message.length()){
+        //Will be handled later also
+        fputs("send() sent an unexpected number of bytes",stderr);
+    }
+
+}
