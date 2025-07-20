7220feaa2e13456f96869a934a4a61252670b664
AnsahOfei <ansahofei.kwadwo@outlook.com>
2025-07-20 17:58:04 +0000

    finished TCP Echo Server constructor definitions

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <string.h>
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,6 +15,34 @@ class TCPServer{
         TCPServer(int port);
         ~TCPServer();
         void start();
-    
-
 };
+
+TCPServer::TCPServer(int port){
+    //incoming connections
+    listening_socket_fd = socket(AF_INET,SOCK_STREAM,IP_PROTOCOL);
+    if (listening_socket_fd < 0){
+        perror("socket() failed");
+        exit(EXIT_FAILURE);        
+    }
+
+    //construct local address
+    sockaddr_in servAddr;
+    memset(&servAddr,0,sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(port);
+
+    //Bind and listen
+    if(bind(listening_socket_fd, (sockaddr*)&servAddr, sizeof(servAddr)) < 0){
+        perror("bind() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    const int MAX_PENDING_CONNECTIONS = 5;
+    if(listen(listening_socket_fd,MAX_PENDING_CONNECTIONS)<0){
+        perror("listen() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server is listening on port" << port << std::endl;
+}
